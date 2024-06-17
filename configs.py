2aae10033a8e6a7476c981b4a5d000f5d9ffa5dc
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-06-17 20:50:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,21 +1,21 @@
-# (c) @JAsuran2p0
+# (c) @AbirHasan2005
 
 import os
 
 
 class Config(object):
-	API_ID = 1923471
-	API_HASH = "fcdc178451cd234e63faefd38895c991"
-	BOT_TOKEN = "7031870365:AAGVTsJcD7JChbTUMQBepqRCS_gmGGpjwus"
-	BOT_USERNAME = "publicmoviesharebot"
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001814192750"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "moviekoodu")
-	LOG_CHANNEL = -1001814192750
+	API_ID = int(os.environ.get("API_ID", 21608308))
+	API_HASH = os.environ.get("API_HASH", "decdf8683a924e72220a94e49ed8d3ce")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6510386769:AAETMNt3z9oOAvjSFzlQ0g4rFmrcK70-oiI")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "ThammuTv_T_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001888985177"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1904146512"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "TshaTTv")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001352606124")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
@@ -28,22 +28,22 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
 
-📡 **Hosted on:** [Render](https://render.com)
+📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @JAsuran2p0
+🧑🏻‍💻 **Developer:** @T3shaM
 
-👥 **Support Group:** [Catchme](https://t.me/jasuranbots)
+👥 **Support Group:** [Linux Repositories](https://t.me/ThammuTV)
 
-📢 **Updates Channel:** [JAsuran Serials](https://t.me/JAsuranserials)
+📢 **Updates Channel:** [Discovery Projects](https://t.me/ThammuTV)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @JAsuran2p0
+🧑🏻‍💻 **Developer:** @T3shaM
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/) (PayPal)
+[Donate Now]() (PayPal)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
